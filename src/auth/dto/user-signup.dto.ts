304a192ed50df5b5cc205de2147ea570f7fca185
YlanDecode <rabenaly3@gmail.com>
2024-08-16 14:01:20 +0300

    feat: user signup dto

--- src/auth/dto/user-signup.dto.ts
+++ src/auth/dto/user-signup.dto.ts
@@ -1,16 +1,40 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
-import { UserEntity } from '../../entity/user.entity';
+import { IsEmail, IsNotEmpty, MinLength, IsEnum, IsDateString } from 'class-validator';
+import { Role } from '../../entity/role.enum';
 
-export class UserSignupDto extends UserEntity {
+export class UserSignupDto {
   @IsNotEmpty()
   id: string;
   @IsNotEmpty()
-  name: string;
-  @IsEmail()
-  email: string;
+  firstName: string;
+  username: string;
   @IsNotEmpty()
   lastName: string;
+
+  @IsEmail()
+  email: string;
+
   @IsNotEmpty()
   @MinLength(8)
   password: string;
+
+  @IsNotEmpty()
+  address: string;
+
+  @IsNotEmpty()
+  @IsEnum(['MALE', 'FEMALE'])
+  gender: 'MALE' | 'FEMALE';
+
+  @IsNotEmpty()
+  @IsDateString()
+  birthDate: string;
+
+  @IsNotEmpty()
+  birthPlace: string;
+
+  @IsNotEmpty()
+  @MinLength(14)
+  phone: string;
+
+  @IsEnum(Role, { each: true })
+  role: Role[];
 }
