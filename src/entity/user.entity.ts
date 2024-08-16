08dc2a403afe67803705c95587748a205180144d
YlanDecode <rabenaly3@gmail.com>
2024-08-16 13:59:15 +0300

    feat: integrated new field

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,25 +1,50 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+} from 'typeorm';
 import { Role } from './role.enum';
+import { LocationEntity } from './location.entity';
 
 @Entity('users')
-export abstract class UserEntity {
+export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({ type: 'boolean', default: true })
   isActive: boolean;
 
-  @Column({ type: 'boolean', default: false })
-  isLogged: boolean;
-
   @Column({ type: 'varchar', length: 250 })
   email: string;
 
-  @Column({ type: 'varchar', length: 250 })
-  name: string;
+  @Column({ type: 'varchar', length: 250, nullable: true })
+  userName: string;
 
   @Column({ type: 'varchar', length: 250 })
   lastName: string;
+
+  @Column({ type: 'varchar', length: 250 })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 14 })
+  phone: string;
+
+  @Column({ type: 'varchar', length: 500 })
+  address: string;
+
+  @Column({ type: 'enum', enum: ['MALE', 'FEMALE'] })
+  gender: 'MALE' | 'FEMALE';
+
+  @Column({ type: 'date' })
+  birthDate: Date;
+
+  @Column({ type: 'varchar', length: 250 })
+  birthPlace: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  registrationDate: Date;
 
   @Column({
     type: 'enum',
@@ -30,4 +55,8 @@ export abstract class UserEntity {
 
   @Column({ type: 'varchar', length: 250 })
   password: string;
+
+  @OneToOne(() => LocationEntity, { cascade: true, eager: true })
+  @JoinColumn()
+  location: LocationEntity;
 }
