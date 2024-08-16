6703b8f08e4bed6b33f241736f0a95ceb443a3b2
YlanDecode <rabenaly3@gmail.com>
2024-08-16 21:32:56 +0300

    feat: places module

---
+++ src/entity/places.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+} from 'typeorm';
+import { LocationEntity } from './location.entity';
+
+@Entity()
+export class PlacesEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 250 })
+  name: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @OneToOne(() => LocationEntity, { cascade: true, eager: true })
+  @JoinColumn()
+  location: LocationEntity;
+}
