6170999e4762242fe0767b9f2f55d54bfd180d6e
YlanDecode <rabenaly3@gmail.com>
2024-08-16 14:02:25 +0300

    feat: user whoami type

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { UserEntity } from '../entity/user.entity';
 import { JwtAuthGuard } from '../core/jwt-auth.guard';
 import { CurrentUser } from '../core/decorators/user.decorator';
 import { Roles } from '../core/decorators/roles.decorator';
 import { Role } from '../entity/role.enum';
+import { UserProfile } from '../auth/model/userprofile.model';
 
 @Controller('users')
 export class UserController {
   @UseGuards(JwtAuthGuard)
   @Get('whoami')
   @Roles(Role.USER)
-  getProfile(@CurrentUser() user: UserEntity) {
+  getProfile(@CurrentUser() user: UserProfile) {
     return user;
   }
 }
